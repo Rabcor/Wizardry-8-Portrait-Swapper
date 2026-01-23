2e8495508e7f2c85d01fd90f029b84268fd33716
Rabcor <rabcor1@hotmail.com>
2026-01-23 17:26:27 +0000

    Update ETRLE.py
    Improved accuracy, should now decompress properly according to the sub_header, it seems able to decompress any STI accurately like this. 
    
    Compression is a bit more accurate, and faster as well but i haven't quite worked out how to re-compress images that have variable dimensions and/or use offsets. Those things are irrelevant to portraits though and probably won't be improved, the old STI-Edit app can be used instead, it's ui is a bit shit but it can save any sti.

--- ETRLE.py
+++ ETRLE.py
@@ -1,88 +1,96 @@
-def etrle_decompress(compressed_image, palette, width=90, height=720):
-    ptr = compressed_image
+def etrle_decompress(compressed_image, palette, header):
+    ptr = bytearray(compressed_image)
     alpha_mask = bytes(palette[0] + (0,))
     image = bytearray()
-    row_width = 0
-    current_row_pos = 0
-    current_row = 0
-    
-    while current_row < height and ptr:
-        run = ptr[0]
-        run_len = run & 0x7F
-        ptr = ptr[1:]
+    index = 0
+
+    for header_entry in header:
+        width = header_entry['width']
+        height = header_entry['height']
         
-        if run & 0x80:
-            image.extend(alpha_mask * run_len)
-            current_row_pos += run_len
-        else:
-            for _ in range(run_len):
-                if not ptr:
-                    break
-                idx = ptr[0]
-                ptr = ptr[1:]
-                image.extend(palette[idx] + (255,))
-                current_row_pos += 1 
-                
-        if current_row_pos >= width:
-            current_row += 1
-            current_row_pos = 0    
-            
-    return image  
+        for row in range(height):
+            row_width = 0
+            while row_width < width and index < len(ptr):
+                run = ptr[index]
+                index += 1
+                run_len = run & 0x7F
 
-def etrle_compress(image, palette, width=640, height=480, sub_width=90):
-    compressed = bytearray()
-    pixel_idx = 0
-    col = 0
-    while pixel_idx < len(image):
+                if run & 0x80:
+                    image.extend(alpha_mask * run_len)
+                    row_width += run_len
+                else:
+                    fill_data = bytearray()
+                    for _ in range(run_len):
+                        if index >= len(ptr):
+                            break
+                        idx = ptr[index]
+                        index += 1
+                        fill_data.extend(palette[idx] + (255,))
+                    image.extend(fill_data)
+                    row_width += run_len
+
+    return bytes(image)
+
+def etrle_compress(image, palette, header):
+    palette_lookup = {}
+    for idx, (r, g, b) in enumerate(palette):
+        palette_lookup[(r, g, b)] = idx
     
-        if image[pixel_idx + 3] == 0:
-            count = 0
-            temp_idx = pixel_idx
-            while temp_idx < len(image) and image[temp_idx + 3] == 0:
-                count += 1
-                temp_idx += 4
-                
-            while count > 0:
-                run_len = min(count, sub_width - col)
-                compressed.append(0x80 | run_len)
-                col += run_len
-                count -= run_len
-                if col >= sub_width:
-                    compressed.append(0x00)
-                    col = 0
+    compressed = bytearray()
+    
+    for header_entry in header:
+        width = header_entry['width']
+        height = header_entry['height']
+        
+        pixel_idx = 0
+        col = 0
+        
+        for row in range(height):
+            row_width = 0
+            while row_width < width and pixel_idx < len(image):
+                if image[pixel_idx + 3] == 0:
+                    count = 0
+                    temp_idx = pixel_idx
+                    while temp_idx < len(image) and image[temp_idx + 3] == 0:
+                        count += 1
+                        temp_idx += 4
+                        
+                    while count > 0:
+                        run_len = min(count, width - col)
+                        compressed.append(0x80 | run_len)
+                        col += run_len
+                        count -= run_len
+                        if col >= width:
+                            compressed.append(0x00)
+                            col = 0
 
-            pixel_idx = temp_idx
-        else:
-            literal_count = 0
-            temp_idx = pixel_idx
-            while (temp_idx < len(image) and 
-                image[temp_idx + 3] != 0 and
-                literal_count < (sub_width - col)):
-                literal_count += 1
-                temp_idx += 4
+                    pixel_idx = temp_idx
+                else:
+                    literal_count = 0
+                    temp_idx = pixel_idx
+                    while (temp_idx < len(image) and 
+                        image[temp_idx + 3] != 0 and
+                        literal_count < (width - col)):
+                        literal_count += 1
+                        temp_idx += 4
 
-            compressed.append(literal_count)
-            col += literal_count
+                    compressed.append(literal_count)
+                    col += literal_count
 
-            temp_idx = pixel_idx
-            for j in range(literal_count):
-                r, g, b = image[temp_idx:temp_idx+3]
-                best_idx = 0
-                best_dist = float('inf')
-                for idx, (pr, pg, pb) in enumerate(palette):
-                    dist = (r - pr)**2 + (g - pg)**2 + (b - pb)**2
-                    if dist < best_dist:
-                        best_dist = dist
-                        best_idx = idx
-                compressed.append(best_idx)
-                temp_idx += 4
+                    temp_idx = pixel_idx
+                    for j in range(literal_count):
+                        r, g, b = image[temp_idx], image[temp_idx + 1], image[temp_idx + 2]
+                        best_idx = palette_lookup.get((r, g, b), 0)
+                        compressed.append(best_idx)
+                        temp_idx += 4
 
-            pixel_idx = temp_idx
-            if col >= sub_width:
-                compressed.append(0x00)
-                col = 0
+                    pixel_idx = temp_idx
+                    if col >= width:
+                        compressed.append(0x00)
+                        col = 0
 
-    if col > 0:
-        compressed.append(0x00)
+        if col > 0:
+            compressed.append(0x00)
+            col = 0
 
-    return compressed 
+    return compressed
