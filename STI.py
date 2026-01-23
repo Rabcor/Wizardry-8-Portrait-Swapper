4d5cfafdf80e330600dbd302b2e4296e4c665a4c
Rabcor <rabcor1@hotmail.com>
2026-01-23 17:29:49 +0000

    Update STI.py
    altered __str__ to be more friendly to SLFEX,, some variable renaming and  adjustments to account for the changed etrle compress and decompress functions.

--- STI.py
+++ STI.py
@@ -8,7 +8,7 @@ from ETRLE import etrle_compress, etrle_decompress
 class STI8:
     def __init__(self, source: str | bytes | None = None):   
         self.transparent, self.high, self.indexed, self.zlib, self.etrle = False, False, True, False, True
-        self.size_real = self.size_uncompressed = self.size_compressed = 0
+        self.size_uncompressed = self.num_pixels = self.size_compressed = 0
         self.height = 480
         self.width = 640
         self.num_colors = 256
@@ -18,6 +18,7 @@ class STI8:
         self.sub_header = self.images = []
         self.atlas = bytes()
         self.modified = False
+        self.index = 0 # Only used in __str__.
 
         if source is not None:
             header_size = 64
@@ -25,7 +26,7 @@ class STI8:
             if data[:4] != b'STCI':
                 raise ValueError("Invalid STI file header")
             flags = struct.unpack('<I', data[16:20])[0]
-            self.size_uncompressed = struct.unpack('<I', data[4:8])[0]
+            self.num_pixels = struct.unpack('<I', data[4:8])[0]
             self.size_compressed = struct.unpack('<I', data[8:12])[0]
             self.transparent, self.high, self.indexed, self.zlib, self.etrle = ((flags >> i) & 1 for i in (0, 2, 3, 4, 5))
             self.height, self.width = struct.unpack('<HH', data[20:24])
@@ -39,7 +40,6 @@ class STI8:
             img_start= sub_header_start + self.num_images * 16
             self.atlas = data[img_start:]
             try:
-                if self.etrle: self.atlas = etrle_decompress(self.atlas,self.palette,self.width, self.height)
                 for i in range(self.num_images):
                     offset = sub_header_start + i * 16
                     image_start = struct.unpack('<I', data[offset:offset+4])[0]
@@ -49,15 +49,21 @@ class STI8:
                     w = struct.unpack('<H', data[offset+14:offset+16])[0]
                     h = struct.unpack('<H', data[offset+12:offset+14])[0]
                     self.sub_header.append({'offset': image_start, 'size': size, 'x': x, 'y': y, 'height': h, 'width': w})
-                    self._split_atlas()
+                self.atlas = etrle_decompress(self.atlas,self.palette,self.sub_header)
+                self._split_atlas()
+                self.size_uncompressed = sum(self.sub_header[i]['width'] * self.sub_header[i]['height'] * 3 for i in range(self.num_images))
             except Exception as e:
                 raise ValueError("Broken or unsupported file")
                 
     def _split_atlas(self):
         self.images = []
-        realsize = self.sub_header[0]['width'] * self.sub_header[0]['height'] * 4
-        for i in range(self.num_images):
-            self.images.append(self.atlas[realsize * i:realsize * i + realsize])
+        offset = 0
+        for entry in self.sub_header:
+            width = entry['width']
+            height = entry['height']
+            size = width * height * 4
+            self.images.append(self.atlas[offset:offset + size])
+            offset += size   
         
     def _join_atlas(self):
         realsize = self.sub_header[0]['width'] * self.sub_header[0]['height'] * 4
@@ -82,10 +88,15 @@ class STI8:
                 if i == 5: self.sub_header[i]['size'] = self.sub_header[i-1]['size'] # Ugly fix
                 if i == 9: self.sub_header[i]['size'] = self.sub_header[i-1]['size'] # This is what regret looks like
                 current_pos = next_start
+            self.size_uncompressed = sum(self.sub_header[i]['width'] * self.sub_header[i]['height'] * 3 for i in range(self.num_images))
         else:
             raise ValueError("Invalid File")
                 
     def _fix_alpha(self): # Ensure transparent pixels are the correct color and black values are 1,1,1 instead of 0,0,0 because 0,0,0 is actually also alpha, which has lead to a great deal of grief...
+        for i in range(len(self.sub_header)):
+            if self.sub_header[i]['width'] != self.sub_header[0]['width'] or self.sub_header[i]['height'] != self.sub_header[0]['height']:
+                print("Code not implemented for inconsistent image dimensions, result may be seriously broken.")
+                return
         width = self.sub_header[0]['width']
         height = len(self.atlas) // (4 * width)
         buffer = np.frombuffer(self.atlas, dtype=np.uint8).reshape(height, width, 4)
@@ -107,20 +118,17 @@ class STI8:
         img_array = np.array(rgb_atlas)
         pixels = img_array.reshape(-1, 3)
         unique_pixels = np.unique(pixels.view([('', pixels.dtype)] * pixels.shape[1])).view(pixels.dtype).reshape(-1, pixels.shape[1])
-        
-        # Remove alpha and end values if they exist in unique_pixels
+
         alpha_array = np.array(alpha)
         end_array = np.array(end)
         
-        # Filter out alpha and end values if present
         mask = ~(np.all(unique_pixels == alpha_array, axis=1) | np.all(unique_pixels == end_array, axis=1))
         unique_pixels = unique_pixels[mask]
         
-        # Check if we need padding
         color_count = len(unique_pixels)
     
-        if color_count > 254: # Quantize
-            print("\tInfo: Quantizing image")
+        if color_count > 254:
+            print("\tInfo: Quantizing image...")
             try: # To use libimagequant for quantization
                 import imagequant
                 output_indices, output_palette = imagequant.quantize_raw_rgba_bytes(
@@ -149,16 +157,14 @@ class STI8:
                         rgba_bytes[i*4+3]   = 255  # Opaque by default
                     else:
                         rgba_bytes[i*4:i*4+4] = [0, 0, 0, 0]  # Fallback
-            
-                # Apply custom alpha replacement (if any pixel matches `alpha` RGB and has A=0)
+                
+                # Fix alpha
                 for i in range(0, len(self.atlas), 4):
                     r, g, b, a = self.atlas[i], self.atlas[i+1], self.atlas[i+2], self.atlas[i+3]
                     if r == alpha[0] and g == alpha[1] and b == alpha[2] and a == 0:
                         rgba_bytes[i:i+4] = [alpha[0], alpha[1], alpha[2], 0]
             
                 self.atlas = bytes(rgba_bytes)
-            
-                # Insert special colors into palette
                 palette.insert(0, alpha)
                 palette.append(end)
                 self.palette = palette
@@ -166,18 +172,18 @@ class STI8:
             except: # Fallback to pillow for quantization, not only is the quantization method worse, the code is also worse. Works tho, just generates suboptimal palettes.
                 print("\tWarning: Quantizing with libimagequant failed, falling back to pillow...")
                 palette_img = rgb_atlas.quantize(colors=254, method=1)
-                palette = palette_img.getpalette()[:254*3]  # Get first 254*3 values (RGB triplets)
+                palette = palette_img.getpalette()[:254*3]
                 palette = [tuple(palette[i:i+3]) for i in range(0, len(palette), 3)]
                 palette.insert(0, alpha)
                 palette[1] = (255,0,0)
                 palette.append(end)
                 self.atlas = palette_img.convert('RGBA').tobytes()
                 
-                # Make alpha color transparent
+                # Fix alpha
                 atlas_list = list(self.atlas)
                 for i in range(0, len(atlas_list), 4):
                     if (atlas_list[i], atlas_list[i+1], atlas_list[i+2]) == alpha:
-                        atlas_list[i+3] = 0  # Set alpha to 0 (transparent)
+                        atlas_list[i+3] = 0 
                         
                 self.atlas = bytes(atlas_list)
                 self.palette = palette
@@ -199,20 +205,19 @@ class STI8:
         self._join_atlas()
         self._fix_alpha()
         self._quantize_atlas()
-        self._split_atlas()
-               
+        self._split_atlas()    
         
     def save(self, filename: str = None):
         self._update()
-        img_data = etrle_compress(self.atlas, self.palette, self.width, self.height, self.sub_header[0]['width'])
+        img_data = etrle_compress(self.atlas, self.palette, self.sub_header)
         self._update_subheader(img_data)
-        self.size_uncompressed = self.width * self.height
+        self.num_pixels = self.width * self.height
         self.size_compressed = len(img_data)
         flags = (self.transparent | (self.high << 2) | (self.indexed << 3) | (self.zlib << 4) | (self.etrle << 5))
 
         header = (
             b'STCI' +
-            struct.pack('<II', self.size_uncompressed, self.size_compressed) +
+            struct.pack('<II', self.num_pixels, self.size_compressed) +
             b'\x00' * 4 +
             struct.pack('<IHHIHBBB', flags, self.height, self.width, self.num_colors, self.num_images, self.r_depth, self.g_depth, self.b_depth) +
             b'\x00' * 11 +
@@ -251,28 +256,27 @@ class STI8:
         if self.indexed: flags.append("Indexed")
         if self.zlib: flags.append("ZLIB")
         if self.etrle: flags.append("ETRLE")
-        flags_str = "\n\t".join(flags) if flags else "None"
+        flags_str = "\n            ".join(flags) if flags else "None"
         
-        if self.size_compressed == self.size_uncompressed:
+        if self.size_compressed == self.num_pixels:
             image_size_str = f"Image Size: {self.size_compressed}"
         else:
-            image_size_str = f"Sizes:\n\tCompressed Atlas:    {self.size_compressed}\n\tUncompressed Atlas:  {self.size_uncompressed}\n\tUncompressed Images: {self.sub_header[0]['width'] * self.sub_header[0]['height'] * 3 * self.num_images}"
-        
+            image_size_str = f"Sizes:\n  Compressed:        {self.size_compressed}\n  Uncompressed:  {self.size_uncompressed}\n  Atlas Size:              {self.num_pixels * 3}"  
         return (
-            f"STI8 File Information:\n"
-            f"{'='*30}\n"
+            f"STI8 Info\n"
             f"Images: {len(self.images)}\n"
             f"Atlas Resolution: {self.width}x{self.height}\n"
-            f"Image Resolution: {self.sub_header[0]['width']}x{self.sub_header[0]['height']}\n"
-            f"Flags:\n\t{flags_str}\n"
+            f"Atlas Pixels: {self.num_pixels}\n"
+            f"Image Resolution: {self.sub_header[self.index]['width']}x{self.sub_header[self.index]['height']}\n"
+            f"Flags:\n            {flags_str}\n"
             f"{image_size_str}\n\n"
             f"Color Information:\n\tColors: {self.num_colors}\n\tBit Depth: {self.bit_depth}\n\t\tR: {self.r_depth}\n\t\tG: {self.g_depth}\n\t\tB: {self.b_depth}\n"
-            f"Palette:\n\t1:   {self.palette[0]} \n\t2:   {self.palette[1]}\n\t3:   {self.palette[2]} \n\t254: {self.palette[253]} \n\t255: {self.palette[254]} \n\t256: {self.palette[255]}\n\n"
-            f"Compressed Atlas Header:\n{chr(10).join(f'\tImage {i+1:2} @{entry['offset']:04x}: {entry['x']},{entry['y']} {entry['width']}x{entry['height']}' + f' - {entry['size']} bytes' for i, entry in enumerate(self.sub_header))}"
+            f"Palette:\n\t1:   {self.palette[0]} \n\t2:   {self.palette[1]}\n\t255: {self.palette[254]} \n\t256: {self.palette[255]}\n\n"
+            f"Compression Header:\n{chr(10).join(f'  Image {i+1:2} @{entry['offset']:04x}:\n    {entry['x']},{entry['y']} {entry['width']}x{entry['height']}' + f' - {entry['size']} bytes' for i, entry in enumerate(self.sub_header))}"
         )
 
 
-class STI16: # Needs some updating, test recreation of images with this.
+class STI16:
     def __init__(self, source: str | bytes | None = None):   
         self.transparent, self.high, self.indexed, self.zlib, self.etrle = False, True, False, False, False
         self.size_uncompressed = self.size_compressed = self.height = self.width = 0
@@ -334,18 +338,18 @@ class STI16: # Needs some updating, test recreation of images with this.
         if self.indexed: flags.append("Indexed")
         if self.zlib: flags.append("ZLIB")
         if self.etrle: flags.append("ETRLE")
-        flags_str = "\n\t".join(flags) if flags else "None"
+        flags_str = "\n            ".join(flags) if flags else "None"
         
         if self.size_compressed == self.size_uncompressed:
-            image_size_str = f"Sizes:\n\tCompressed:   {self.size_compressed}\n\tUncompressed: {self.size_uncompressed}"
+            image_size_str = f"Size: {self.size_uncompressed}"
         else:
-            image_size_str = f"Sizes:\n\tCompressed: {self.size_compressed}\n\tUncompressed: {self.size_uncompressed}"
+            image_size_str = f"Sizes:\n  Compressed:         {self.size_compressed}\n  Uncompressed:   {self.size_uncompressed}"
+            
         
         return (
-            f"STI16 File Information:\n"
-            f"{'='*30}\n"
+            f"STI16 Info\n"
             f"Image Resolution: {self.width}x{self.height}\n"
-            f"Flags:\n\t{flags_str}\n"
-            f"{image_size_str}\n\n"
-            f"Color Information:\n\tColors: {self.num_colors}\n\tBit Depth: {self.bit_depth} (Mask)\n\t\tR: {self.r_depth}  ({self.r_mask:04X})\n\t\tG: {self.g_depth}  ({self.g_mask:04X})\n\t\tB: {self.b_depth}  ({self.b_mask:04X})\n"
+            f"Flags:\n            {flags_str}\n\n"
+            f"{image_size_str}\n"
+            f"Color Information:\n    Colors: {self.num_colors}\n        Bit Depth: {self.bit_depth} (Mask)\n\t      R: {self.r_depth}  ({self.r_mask:04X})\n\t      G: {self.g_depth}  ({self.g_mask:04X})\n\t      B: {self.b_depth}  ({self.b_mask:04X})\n"
         )
