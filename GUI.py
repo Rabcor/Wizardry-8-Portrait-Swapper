9b81c1c5f41fa0f94dd1a60fa41e31e56beb73a8
Rabcor <rabcor1@hotmail.com>
2026-01-31 06:34:14 +0000

    Update GUI.py
    refined graphical element code (no real change for the end-user for that tho) and added automatic screen width based scaling

--- GUI.py
+++ GUI.py
@@ -12,8 +12,12 @@ class GUI:
             self.root = TkinterDnD.Tk()
         except Exception as e:
             self.root = tk.Tk()
+        base_screen_width = 2060 # Controls scaling curve, this is the width at which the window would be rendered at 1:1 scaling.
+        user_screen_width = self.root.winfo_screenwidth()
 
         # Initialize variables
+        self.ui_scale = max(user_screen_width / base_screen_width, 0.75)
+        self.aspect_ratio = 1.25 # Aspect ratio for canvases and images, not the app.
         self.default_portraits = default_portraits
         self.modded_portraits = modded_portraits
         self.medium_image_index = 0
@@ -29,15 +33,14 @@ class GUI:
         self.fg_color = "#ffffff"
         self.button_bg = "#3a3a3a"
         self.highlight_color = "#5a5a5a"
-        self.font_size = 13
+        self.font_size = int(12 + self.ui_scale * 2)
         
         # Window setup
         root = self.root
         root.title("Wizardry 8 Portrait Swapper")
-        if sys.platform == "win32":
-            root.geometry("940x460")
-        else:
-            root.geometry("940x480")
+        width = int(940 * self.ui_scale)
+        height = int(480 * self.ui_scale)
+        root.geometry(f"{width}x{height}")
         root.resizable(False, False)
         root.configure(bg=self.bg_color)
         self.center_window(root)
@@ -54,7 +57,8 @@ class GUI:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         main_frame.columnconfigure(1, weight=1)
-        
+        main_frame.rowconfigure(0, weight=1) 
+       
         # Styles
         style = ttk.Style()
         style.theme_use('alt')
@@ -64,11 +68,11 @@ class GUI:
         
         # Listbox
         list_frame = ttk.Frame(main_frame, style="Dark.TFrame")
-        list_frame.grid(row=0, column=0, sticky=(tk.W, tk.N, tk.S), pady=5)
+        list_frame.grid(row=0, column=0, sticky=(tk.W, tk.N), pady=5)
         list_frame.columnconfigure(0, weight=1)
         list_frame.rowconfigure(0, weight=1)
         
-        self.portrait_listbox = tk.Listbox(list_frame, width=12, height=12, 
+        self.portrait_listbox = tk.Listbox(list_frame, width=int(self.font_size - self.ui_scale * 2), height=int(12 * self.ui_scale), 
                                          bg=self.button_bg, fg=self.fg_color,
                                          selectbackground=self.highlight_color,
                                          selectforeground=self.fg_color,
@@ -82,7 +86,7 @@ class GUI:
         self.portrait_listbox.bind('<<ListboxSelect>>', self.on_portrait_select)
         
         scrollbar = ttk.Scrollbar(list_frame, orient="vertical", command=self.portrait_listbox.yview, style="Dark.Vertical.TScrollbar")
-        scrollbar.grid(row=0, column=1, sticky=(tk.N, tk.S))
+        scrollbar.grid(row=0, column=1, sticky=(tk.NE, tk.SE))
         self.portrait_listbox.config(yscrollcommand=scrollbar.set)
         
         # Image display area
@@ -93,10 +97,16 @@ class GUI:
         image_frame.columnconfigure(2, weight=1)
         
         # Canvas displays
-        self.large_canvas = tk.Canvas(image_frame, width=360, height=288, bg='black', highlightthickness=0)
+        canvas_scale = 2 * self.ui_scale
+        canvas_height = 36 * canvas_scale
+        canvas_width = canvas_height * self.aspect_ratio
+        s_res, m_res, l_res = (canvas_width, canvas_height), (canvas_width * 2, canvas_height * 2), (canvas_width * 4, canvas_height * 4)
+
+        
+        self.large_canvas = tk.Canvas(image_frame, width=l_res[0], height=l_res[1], bg='black', highlightthickness=0)
         self.large_canvas.grid(row=1, column=0, padx=2, pady=0, sticky=tk.N)
                 
-        self.medium_canvas = tk.Canvas(image_frame, width=180, height=144, bg='grey', highlightthickness=0)
+        self.medium_canvas = tk.Canvas(image_frame, width=m_res[0], height=m_res[1], bg='grey', highlightthickness=0)
         self.medium_canvas.grid(row=1, column=1, padx=2, pady=0, sticky=tk.N)
              
         # Medium slider
@@ -104,16 +114,16 @@ class GUI:
                         "Eyes Shocked", "Mouth Neutral", "Mouth Half Open", "Mouth Open", "Mouth Grimace"]
         
         self.medium_slider = tk.Scale(image_frame, from_=0, to=9, orient=tk.HORIZONTAL, 
-                                    command=self.on_medium_slider_change, length=180,
+                                    command=self.on_medium_slider_change, length=180 * self.ui_scale,
                                     label=self.slider_labels[0], showvalue=False,
                                     bg=self.bg_color, activebackground=self.highlight_color, fg=self.fg_color, relief=tk.FLAT,
                                     troughcolor=self.button_bg, sliderrelief=tk.RAISED,
                                     highlightthickness=0, font=("Segoe UI", self.font_size))
-        self.medium_slider.grid(row=1, column=1, padx=5, pady=140, sticky=tk.N)
+        self.medium_slider.grid(row=1, column=1, padx=5, pady=m_res[1], sticky=tk.N)
         self.medium_slider.set(0)
 
         alpha_frame = tk.Frame(image_frame, bg=self.bg_color)
-        alpha_frame.grid(row=1, column=1, padx=2, pady=100, sticky=tk.S)
+        alpha_frame.grid(row=1, column=1, padx=2, pady=m_res[1] + 60, sticky=tk.N)
         
         alpha_label = tk.Label(alpha_frame, text="Alpha Color:", bg=self.bg_color, fg=self.fg_color, font=("Segoe UI", self.font_size))
         alpha_label.pack(side=tk.LEFT)
@@ -121,24 +131,26 @@ class GUI:
         self.alpha_value = tk.Label(alpha_frame, text="", bg=self.bg_color, fg=self.fg_color, font=("Segoe UI", self.font_size))
         self.alpha_value.pack(side=tk.LEFT)
         
-        self.small_canvas = tk.Canvas(image_frame, width=90, height=72, bg='black', highlightthickness=0)
+        self.small_canvas = tk.Canvas(image_frame, width=s_res[0], height=s_res[1], bg='black', highlightthickness=0)
         self.small_canvas.grid(row=1, column=2, padx=2, pady=0, sticky=tk.N)
         
         # Buttons
-        self.save_button = ttk.Button(main_frame, text="Save", command=self.save)
-        self.save_button.grid(row=1, column=0, columnspan=2, pady=10, padx=5, sticky=tk.SE)
+        button_frame = tk.Frame(main_frame, bg=self.bg_color)
+        button_frame.grid(row=1, column=1, pady=0, padx=5, sticky=tk.SE)
         
-        self.change_button = ttk.Button(main_frame, text="Change Portrait", command=self.change_portrait)
-        if sys.platform == "win32":
-            self.change_button.grid(row=1, column=1, pady=10, sticky=tk.SE, padx=130)
-        else:
-            self.change_button.grid(row=1, column=1, pady=10, sticky=tk.SE, padx=140)
+        self.change_button = ttk.Button(button_frame, text="Change Portrait", command=self.change_portrait)
+        self.change_button.grid(row=0, column=0, padx=(0, 5))  
+        
+        self.save_button = ttk.Button(button_frame, text="Save", command=self.save)
+        self.save_button.grid(row=0, column=1, padx=(5, 0))
+
         
         self.extract_button = ttk.Button(main_frame, text="Extract", command=self.extract)
-        self.extract_button.grid(row=1, column=1, pady=10, sticky=tk.SW, padx=5)
+        self.extract_button.grid(row=1, column=1, pady=(15, 0), sticky=tk.SW, padx=(10, 0))
         
         self.defaults_button = ttk.Button(main_frame, text="Restore Defaults", command=self.restore_defaults)
-        self.defaults_button.grid(row=1, column=0, pady=10, sticky=tk.SW, padx=5)
+        self.defaults_button.grid(row=1, column=0, pady=(0, 0), sticky=tk.SW, padx=(10, 0))
+
 
         
         
@@ -186,13 +198,9 @@ class GUI:
 
 
         # Populate listbox
-        self.populate_portrait_listbox()    
-        
-       # Select first item if available
-        if self.portrait_listbox.size() > 0:
-            self.portrait_listbox.selection_set(0)
-            self.current_selection = self.portrait_listbox.get(0)
-            self.load_portraits(self.current_selection)
+        self.populate_portrait_listbox()
+        self.load_portraits(self.current_selection)        
+
              
     def populate_portrait_listbox(self):
         portrait_names = set()
@@ -207,7 +215,9 @@ class GUI:
         sorted_names = sorted(list(portrait_names))
         for name in sorted_names:
             self.portrait_listbox.insert(tk.END, name)
-        
+        self.portrait_listbox.selection_set(0)
+        self.current_selection = self.portrait_listbox.get(0)
+                
     def on_portrait_select(self, event=None):
         self.medium_image_index = 0
         self.medium_slider.set(0)
@@ -268,7 +278,7 @@ class GUI:
     def display_image(self, image_data, canvas, width, height):
         try:
             canvas.delete("all")
-            canvas.config(width=width * 2, height=height * 2)
+            c_width, c_height = int(canvas['width']), int(canvas['height'])
             
             if isinstance(image_data, list):
                 img = Image.new('RGBA', (width, height))
@@ -292,9 +302,9 @@ class GUI:
                 img = Image.new('RGB', (width, height))
                 img.putdata([(r, g, b) for r, g, b in zip(image_data[::3], image_data[1::3], image_data[2::3])])
             
-            img = img.resize((width * 2, height * 2), Image.LANCZOS)
+            img = img.resize((c_width, c_height), Image.LANCZOS)
             photo = ImageTk.PhotoImage(img)
-            canvas.create_image(width, height, image=photo)
+            canvas.create_image(0, 0, image=photo, anchor='nw')
             canvas.image = photo
         except Exception as e:
             print(f"Error displaying image: {str(e)}")
@@ -324,57 +334,55 @@ class GUI:
             if not files:return
         files = sorted(files)    
         index = 0
-        large_modified = False
         medium_modified = False
-        small_modified = False
         
         # Process each file
         for file_path in files:
-            filename, extension = os.path.splitext(file_path)
+            filename, extension = os.path.splitext(os.path.basename(file_path))
             if extension.lower() == '.png':
                 try:
                     img = Image.open(file_path)
                     width, height = img.size
                     
-                    # Handle large portraits (180x144)
-                    if width == 180 and height == 144:
-                        if img.mode != 'RGB':
-                            img = img.convert('RGB')
-                        raw_data = list(img.getdata())
-                        self.loaded_sti[0].image = bytes(pixel for rgb in raw_data for pixel in rgb)
-                        large_modified = True
-    
-                    # Handle medium portraits (90x72)
-                    elif width == 90 and height == 72:
-                        if len(files) < 2: index = self.medium_image_index
-                        if img.mode != 'RGBA':
-                            img = img.convert('RGBA')
-                        raw_data = list(img.getdata())
-                        self.loaded_sti[1].images[index] = [pixel for rgb in raw_data for pixel in rgb]
-                        if self.cached_keys[1] == "PORTRAITS\\MEDIUM\\MHUMM4.STI":
-                                self.loaded_sti[1].sub_header[index]['x'] = self.loaded_sti[1].sub_header[index]['y'] = 0
-                                self.loaded_sti[1].sub_header[index]['width'] = width
-                                self.loaded_sti[1].sub_header[index]['height'] = height
-                        medium_modified = True
-                        index += 1                        
-                        
-                    # Handle small portraits (45x36 or 46x36)
-                    elif height == 36 and width in [45, 46]:
-                        if self.loaded_sti[2].high:
+                    if width == height * self.aspect_ratio:
+                        # Handle large portraits (180x144)
+                        if width == 180 and height == 144:
                             if img.mode != 'RGB':
                                 img = img.convert('RGB')
                             raw_data = list(img.getdata())
-                            self.loaded_sti[2].image = bytes(pixel for rgb in raw_data for pixel in rgb)   
-                            self.loaded_sti[2].width = width
-                            self.loaded_sti[2].height = height
-                        elif self.loaded_sti[2].indexed:
+                            self.loaded_sti[0].image = bytes(pixel for rgb in raw_data for pixel in rgb)
+                            self.modded_portraits[self.cached_keys[0]] = self.loaded_sti[0].save()
+        
+                        # Handle medium portraits (90x72)
+                        elif width == 90 and height == 72:
+                            if len(files) < 2: index = self.medium_image_index
                             if img.mode != 'RGBA':
                                 img = img.convert('RGBA')
                             raw_data = list(img.getdata())
-                            self.loaded_sti[2].images[0] = bytes(pixel for rgb in raw_data for pixel in rgb)  
-                            self.loaded_sti[2].sub_header[0]['width'] = width
-                            self.loaded_sti[2].sub_header[0]['height'] = height
-                        small_modified = True
+                            self.loaded_sti[1].images[index] = [pixel for rgb in raw_data for pixel in rgb]
+                            if self.cached_keys[1] == "PORTRAITS\\MEDIUM\\MHUMM4.STI":
+                                    self.loaded_sti[1].sub_header[index].update({'x': 0, 'y': 0, 'width': width, 'height': height})
+                            medium_modified = True
+                            index += 1                        
+                            
+                        # Handle small portraits (45x36 or 46x36)
+                        elif height == 36 and width == 45:
+                            if self.loaded_sti[2].high:
+                                if img.mode != 'RGB':
+                                    img = img.convert('RGB')
+                                raw_data = list(img.getdata())
+                                self.loaded_sti[2].image = bytes(pixel for rgb in raw_data for pixel in rgb)   
+                                self.loaded_sti[2].width = width
+                                self.loaded_sti[2].height = height
+                            elif self.loaded_sti[2].indexed:
+                                if img.mode != 'RGBA':
+                                    img = img.convert('RGBA')
+                                raw_data = list(img.getdata())
+                                self.loaded_sti[2].images[0] = bytes(pixel for rgb in raw_data for pixel in rgb)  
+                                self.loaded_sti[1].sub_header[index].update({'x': 0, 'y': 0, 'width': width, 'height': height})
+                            self.modded_portraits[self.cached_keys[2]] = self.loaded_sti[2].save()
+                        else:
+                            messagebox.showerror("Error", f"Incompatible image resolution: {file_path}")
                     else:
                         messagebox.showerror("Error", f"Incompatible image resolution: {file_path}")
                 except Exception as e:
@@ -387,11 +395,11 @@ class GUI:
                     transparent, high, indexed, zlib, etrle = ((flags >> i) & 1 for i in (0, 2, 3, 4, 5))      
                     sti = STI16(file_path) if high else STI8(file_path) if indexed else None
                     width, height = (sti.width, sti.height) if isinstance(sti, STI16) else (sti.sub_header[0]['width'], sti.sub_header[0]['height'])
-                    if width == 180 and height == 144:
+                    if (width == 180 and height == 144) or filename.startswith('L'):
                         self.loaded_sti[0] = sti
-                    elif width == 90 and height == 72:
+                    elif (width == 90 and height == 72) or filename.startswith('M'):
                         self.loaded_sti[1] = sti                 
-                    elif width in [45, 46] and height == 36 :
+                    elif (height == 36 and width in [45, 46]) or filename.startswith('S'):
                         self.loaded_sti[2] = sti
                     else:
                         messagebox.showerror("Error", f"Incompatible image resolution: {file_path}")
@@ -407,21 +415,15 @@ class GUI:
             self.display_image(self.loaded_sti[2].image, self.small_canvas, self.loaded_sti[2].width, self.loaded_sti[2].height)
         else:
             self.display_image(self.loaded_sti[2].images, self.small_canvas, self.loaded_sti[2].sub_header[0]['width'], self.loaded_sti[2].sub_header[0]['height'])
-        if large_modified:
-            self.modded_portraits[self.cached_keys[0]] = self.loaded_sti[0].save()
         if medium_modified:
             self.modded_portraits[self.cached_keys[1]] = self.loaded_sti[1].save()
-        if small_modified:
-            self.modded_portraits[self.cached_keys[2]] = self.loaded_sti[2].save()
+
+            
       
     def restore_defaults(self):
         # Create custom dialog window
         dialog = tk.Toplevel(self.root)
         dialog.title("Restore Defaults")
-        if sys.platform == "win32":
-            dialog.geometry("430x100")
-        else:
-            dialog.geometry("430x110")
         dialog.configure(bg=self.bg_color)
         dialog.resizable(False, False)
         dialog.transient(self.root)
@@ -482,10 +484,6 @@ class GUI:
         # Create custom dialog for format selection
         dialog = tk.Toplevel(self.root)
         dialog.title("Extract As...")
-        if sys.platform == "win32":
-            dialog.geometry("500x180")
-        else:
-            dialog.geometry("500x200")
         dialog.configure(bg=self.bg_color)
         dialog.resizable(False, False)
         dialog.transient(self.root)
@@ -513,7 +511,7 @@ class GUI:
         dir_frame.pack(pady=10)
     
         dir_entry = tk.Entry(dir_frame, textvariable=save_dir, width=40)
-        dir_entry.pack(side=tk.LEFT, padx=(0, 5))
+        dir_entry.pack(side=tk.LEFT, padx=(20, 20))
         dir_entry.configure(bg=self.bg_color, fg=self.fg_color)
     
         dir_button = ttk.Button(dir_frame, text="Browse", command=select_directory)
